6360eb74930e8db7fbfc8dd99a0861cb1fae5c1c
Eyan Eubanks <eyaneubanks@gmail.com>
2024-11-29 12:22:01 -0800

    Add working example of encryption using linux kernal system call. TODO: - Implement decryption. - Edit for use with arbitrary amount of Bytes. - Use Strace to track system call.

--- SocketDetection/EncryptionProbe.cpp
+++ SocketDetection/EncryptionProbe.cpp
@@ -16,8 +16,9 @@ program will demonstrate a encryption system call an attacker may use.
 #include <sys/random.h>
 #include <sys/socket.h> // Used for socke handeling
 #include <linux/if_alg.h>   // Defines struct (sockaddr_alg) for interface with encrypting over socket
+#include <poll.h>
 
-#define PT_LEN (63 * 1024) // Max number of data used by zero copy interface (63 kilobytes * size of Kilobyte )
+#define PT_LEN (63 * 1024) // Max number of data used by zero copy interface (63 kilobytes * size of Kilobyte )[64512]
 #define CT_LEN PT_LEN
 #define IV_LEN 16
 #define KEY_LEN 16
@@ -28,77 +29,210 @@ static uint8_t ct[CT_LEN];  // Cipher Text
 static uint8_t key[KEY_LEN];// Cipher Key
 static uint8_t iv[IV_LEN];  // Cipher Initialization Vector
 
+
+static const unsigned char crypto_key[] = "testtest12345678";
+static const char plain_text[] = "stringtoencrypt0";
+static int opfd = -1, tfmfd = -1;
+static const char algo[] = "ecb(aes)";
+
 int main(void){
 
-    // Fill test data
-    * int key;
-    std::getrandom(pt, sizeof(pt), GRNC_NONBLOCK);
-    std::getrandom(ct, sizeof(ct), GRNC_NONBLOCK);
-    std::getrandom(key, sizeof(key), GRNC_NONBLOCK);
-    std::getrandom(iv, sizeof(iv), GRNC_NONBLOCK);
+    char dst[16] = {0};
+    char afalg_plain[16] = {0};
 
-    // Set up AF_ALG Socket
-    int alg_s, aes_ctr;
-    struct sockaddr_alg sa = { .salg_faimly = AF_ALG};
-    strcpy(sa.salg_type, "skcipher");
-    strcpy(sa.salg_name, "ctr-aes-aesni");
+    struct sockaddr_alg sa = {
+		.salg_family = AF_ALG,
+		.salg_type = "skcipher",
+		.salg_name = "ecb(aes)"
+	};
 
-    alg_s = socket(AF_ALG, SOCK_SEQPACKET, 0);  // AF_ALG determines that socket will be for encryption
-    bind(alg_s,(const struct sockaddr *)&sa, sizeof(sa));   // Assigns an address to a socket using file descriptor.
-    setsockopt(alg_s, SOL_ALG, ALG_SET_KEY, key, KEY_LEN);  // Sets options at the option name and level, defining that this socket will be used for encryption and be used as a key.
-    aes_ctr = accept(alg_s, NULL, NULL);    // Controller that accepts connection on the socket. Returns file descriptor used to identify socket.
-
-    // Set up Initializaiton Vector (IV)
-    // Need ssize_t sendmsg (int socket, const struct msghdr *message, int flags);
-    uint_8_t cmsg_buf[CMSG_SPACE(sizeof(uint32_t)) + CMSG_SPACE(sizeof(struct af_alg_iv) + IV_LEN)] = {0};  // (cmsghdr + uint32) + (cmsghdr + (algorithm IV struct{ivlen, and iv[]} + Bytes of IV)). Defines space needed to store IV vector pairs [cmsghdr, dataarray[]]. 
-    struct msghdr msg{          // Need to construct mghdr message
-        .msg_control = cmsg_buf,
-        .msg_controllen = sizeof(csmg_buf)
-    };
-
-    // Setup first sequence of cmsghdr to store operations
-    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);     // Returns pointer to cmsghdr struct from msghdr to set cmsghdr attributes
-    cmsg->cmsg_len = CMSG_LEN(sizeof(uint32_t));    // data byte count including cmsghdr
-    cmsg->cmsg_level = SOL_ALG;                     // Symetric encryption algorithm protocol
-    cmsg->cmsg_type = ALG_SET_OP;                   // Socket performs operations.
-    *((uint32_t *)CMSG_DATA(cmsg)) = ALG_OP_ENCRYPT;// Operation of Encrypt Or Decrypt. Points to data array in Ancillary data
-
-    // Setups up next sequence of cmsghdr structure
-    cmsg = CMSG_NXTHDR(&msg, cmsg);                 // Cmsg points to next sequence of cmsghdr.
-    cmsg->csmg_len = CMSG_LEN(sizeof(struct af_alg_iv) + IV_LEN);   // Af_alg_iv struct + 16 Bytes.
-    cmsg->cmsg_level = SOL_ALG;
-    cmsg->cmsg_type = ALG_SET_IV;
-    ((struct af_alg_iv *))CMSG_DATA(cmsg)->ivlen = IV_LEN           // Set IV length to 16 Bytes.
-    memcpy(((struct af_alg_iv *)CMSG_DATA(cmsg))-> iv, iv, IV_LEN); // copies data from iv array into CMSG data array which is 16 Bytes.
-    sendmsg(aes_ctr, &msg, 0);                                      // Sends system call to kernal space.
-
-    // Set up pipes for Zero-Copy interface
-    int pipes[2];   // File descriptors that point at files
-    pipe(pipes);
-
-    // Input Output Structure. 
-    struct iovec pt_iov = {
-        .iov_base = pt;
-        .iov_len = sizeof(pt);
+	tfmfd = socket(AF_ALG, SOCK_SEQPACKET, 0);
+	if (tfmfd == -1){
+	    perror("Socket failed");
+        return 1;
     }
+	if (bind(tfmfd, (struct sockaddr *)&sa, sizeof(sa)) == -1){
+        perror("bind failed");
+        return 1;
+    }
+	if (setsockopt(tfmfd, SOL_ALG, ALG_SET_KEY, crypto_key, 16) == -1){
+        perror("setting options of socket failed");
+        return 1;
+    }
+	opfd = accept(tfmfd, NULL, 0);
+	if (opfd == -1){
+        perror("accepting connections failed");
+        return 1;
+    }
+    struct msghdr msg = {};
+	struct cmsghdr *cmsg;
+	char cbuf[CMSG_SPACE(4)] = {0};
+	struct iovec iov;
+
+	msg.msg_control = cbuf;
+	msg.msg_controllen = sizeof(cbuf);
+
+	cmsg = CMSG_FIRSTHDR(&msg);
+	cmsg->cmsg_level = SOL_ALG;
+	cmsg->cmsg_type = ALG_SET_OP;
+	cmsg->cmsg_len = CMSG_LEN(4);
+	*(__u32 *)CMSG_DATA(cmsg) = ALG_OP_ENCRYPT;
+
+	iov.iov_base = (char *)plain_text;
+	iov.iov_len = sizeof(dst);
+
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	sendmsg(opfd, &msg, 0);
+	printf("The amount of bytes read are :%ld. The characters stored in dst are: %s",read(opfd, dst, sizeof(dst)), dst);
+
+    
+    // // Fill test data
+    // getrandom(pt, sizeof(pt), GRND_NONBLOCK);
+    // getrandom(ct, sizeof(ct), GRND_NONBLOCK);
+    // getrandom(key, sizeof(key), GRND_NONBLOCK);
+    // getrandom(iv, sizeof(iv), GRND_NONBLOCK);
+
+    // // Set up AF_ALG Socket
+    // int alg_s, aes_ctr;
+    // struct sockaddr_alg sa = {
+    //     .salg_family = AF_ALG,
+    //     .salg_type = "skcipher",
+    //     .salg_name = "ecb(aes)"
+    // };
+    
+    // // strcpy(sa.salg_type, "skcipher");
+    // // strcpy(sa.salg_name, "ctr-aes-aesni");
+
+    // alg_s = socket(AF_ALG, SOCK_SEQPACKET, 0);  // AF_ALG determines that socket will be for encryption
+    // if(alg_s < 0){
+    //     perror("Socket failed");
+    //     return 1;
+    // }
+    // int bind_result = bind(alg_s,(struct sockaddr *)&sa, sizeof(sa));   // Assigns an address to a socket using file descriptor.
+    // if(bind_result < 0){
+    //     perror("bind failed");
+    //     return 1;
+    // }
+    // int sockset_result = setsockopt(alg_s, SOL_ALG, ALG_SET_KEY, key, KEY_LEN);  // Sets options at the option name and level, defining that this socket will be used for encryption and be used as a key.
+    // if(sockset_result < 0){
+    //     perror("setting options of socket failed");
+    //     return 1;
+    // }
+    // aes_ctr = accept(alg_s, NULL, 0);    // Controller that accepts connection on the socket. Returns file descriptor used to identify socket.
+    // if(aes_ctr < 0){
+    //     perror("accepting connections failed");
+    //     return 1;
+    // }
+    // // int notClosed = close(alg_s);
+    // // if(notClosed){
+    // //     perror("Socket closing failed");
+    // //     return 1;
+    // // }
+
+    // // Set up Initializaiton Vector (IV)
+    // // Need ssize_t sendmsg (int socket, const struct msghdr *message, int flags); msg_control described in Socket.c
+    // uint8_t cmsg_buf[CMSG_SPACE(sizeof(uint32_t)) + CMSG_SPACE(sizeof(struct af_alg_iv)/* + IV_LEN*/)] = {0};  // (cmsghdr + uint32) + (cmsghdr + (algorithm IV struct{ivlen, and iv[]} + Bytes of IV)). Defines space needed to store IV vector pairs [cmsghdr, dataarray[]]. 
+    // struct msghdr msg = {          // Need to construct mghdr message
+    //     .msg_control = cmsg_buf,
+    //     .msg_controllen = sizeof(cmsg_buf)
+    // };
+
+    // // Setup first sequence of cmsghdr to store operations
+    // struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);     // Returns pointer to cmsghdr struct from msghdr to set cmsghdr attributes
+    // cmsg->cmsg_len = CMSG_LEN(sizeof(uint32_t));    // data byte count. This is size of "ALG_OP_ENCRYPT" which is an uint32.
+    // cmsg->cmsg_level = SOL_ALG;                     // Symetric encryption algorithm protocol
+    // cmsg->cmsg_type = ALG_SET_OP;                   // Socket performs operations.
+    // *((uint32_t *)CMSG_DATA(cmsg)) = ALG_OP_ENCRYPT;// Operation of Encrypt Or Decrypt. Points to data array in Ancillary data
+
+    // // Setups up next sequence of cmsghdr structure
+    // cmsg = CMSG_NXTHDR(&msg, cmsg);                 // Cmsg points to next sequence of cmsghdr.
+    // cmsg->cmsg_len = CMSG_LEN(sizeof(struct af_alg_iv)/* + IV_LEN*/);   // Af_alg_iv struct + 16 Bytes.
+    // cmsg->cmsg_level = SOL_ALG;
+    // cmsg->cmsg_type = ALG_SET_IV;
+    // ((struct af_alg_iv *)CMSG_DATA(cmsg))->ivlen = IV_LEN;          // Set IV length to 16 Bytes.
+    // memcpy(((struct af_alg_iv *)CMSG_DATA(cmsg))->iv, iv, IV_LEN);  // copies data from iv array into CMSG data array which is 16 Bytes.
+
+    // // Input Output Structure. Stores buffer of plain text for pipe to access.
+    // struct iovec pt_iov = {
+    //     .iov_base = pt,
+    //     .iov_len = PT_LEN
+    // };
+
+    // msg.msg_iov = &pt_iov;
+    // msg.msg_iovlen = 1;
+
+    // // int flags = fcntl(aes_ctr, F_GETFL, 0);
+    // // if(flags == -1){
+    // //     perror("fcntl F_GETFL");
+    // // }
+    // // if (fcntl(aes_ctr, F_SETFL, flags | O_NONBLOCK) == -1) {
+    // // perror("fcntl F_SETFL");
+    // // }
+
+    // printf("Now sending message\n");
+    // if(-1 == sendmsg(aes_ctr, &msg, MSG_DONTWAIT)){
+    //     perror("error with Socket");
+    // }
+
+    // END OF COMMENT********************************************** */ 
+    // // printf("Bytes sent were: %d",bytesSent );                                 // Sends system call to kernal space.
+    // // read(aes_ctr, ct, CT_LEN);
+
+    // // // Set up pipes for Zero-Copy interface
+    // // int pipes[2];   // File descriptors that point at files
+    // // pipe(pipes);
+
+
+    // printf("Finished I/O structure\n");
+
 
     // Call system call multiple times for Block encryption
-    int i;
-    for(int i = 0; i < ITER_COUNT; i++){
-        vmsplice(pipes[1],&pt_iov, 1, SPLICE_F_GIFT);    // Takes output/ writing interface of pipe and maps input/output vector segment into a pipe.
-        splice(pipes[0], NULL, aes_ctr, NULL, sizeof(pt), SPLICE_F_MORE);   // Takes input of pipe and sends to aes contol socket. SPLICE_F_MORE means more data is comming
-        read(aes_ctr, ct, sizeof(ct));  // Takes aes pointer/ file descriptor buffer and store into cipher text buffer.
-    }
-    // Final call to read data.
-    vmsplice(pipes[1],&pt_iov, 1, SPLICE_F_GIFT);
-    splice(pipes[0], NULL, aes_ctr, NULL, sizeof(pt), 0);
-    read(aes_ctr, ct, sizeof(ct));
+    // int i;
+    // for(i = 0; i < ITER_COUNT; i++){
+    //     vmsplice(pipes[1],&pt_iov, 1, SPLICE_F_GIFT);    // Takes output/ writing interface of pipe and maps input/output vector segment into a pipe.
+    //     splice(pipes[0], NULL, aes_ctr, NULL, sizeof(pt), SPLICE_F_MORE);   // Takes input of pipe and sends to aes contol socket. SPLICE_F_MORE means more data is comming
 
-    // Close file descriptors connected to open pipes and sockets 
-    close(pipes[0]);
-    close(pipes[1]);
-    close(aes_ctr);
+    //     struct pollfd pfd;
+    //     pfd.fd = aes_ctr,       // Socket file descriptor
+    //     pfd.events = POLLIN,    // Interested in read events (POLLIN)
+    //     pfd.revents = 0;        // will be set by kernal poll()
 
+    //     int ret = poll(&pfd, 1, 1000);    // Timeout is in milliseconds.
+    //     if(ret > 0){
+    //         if(pfd.revents & POLLIN){    // Mask of kernal sigifying polling.
+    //             // Data avaliable to be read
+    //             int bytes_read = read(aes_ctr, ct, sizeof(ct));
+    //             if(bytes_read > 0){
+    //                 printf("Bytes have been read: %c\n", ct[i]);
+    //             }else if(bytes_read == 0){
+    //                 printf("Socket has no more data");
+    //             }else{
+    //                 printf("Error reading from socket");
+    //             }
+    //         }
+    //     } else if(ret == 0){
+    //         printf("Timeout: No data avaliable on the socket");
+    //     } else{ 
+    //         printf("Error in Poll()");
+    //     }
+
+
+    //     //vmsplice(pipes[1],&pt_iov, 1, SPLICE_F_GIFT);    // Takes output/ writing interface of pipe and maps input/output vector segment into a pipe.
+    //     //splice(pipes[0], NULL, aes_ctr, NULL, sizeof(pt), SPLICE_F_MORE);   // Takes input of pipe and sends to aes contol socket. SPLICE_F_MORE means more data is comming
+    //     //read(aes_ctr, ct, sizeof(ct));  // Takes aes pointer/ file descriptor buffer and store into cipher text buffer.
+    // }
+    // // // Final call to read data.
+    // // vmsplice(pipes[1],&pt_iov, 1, SPLICE_F_GIFT);
+    // // splice(pipes[0], NULL, aes_ctr, NULL, sizeof(pt), 0);
+    // // read(aes_ctr, ct, sizeof(ct));
+
+    // // Close file descriptors connected to open pipes and sockets 
+    // close(pipes[0]);
+    // close(pipes[1]);
+    // close(aes_ctr);
+    // printf("all sockets and pipes are closed\n");
 
     return 0;
 }
